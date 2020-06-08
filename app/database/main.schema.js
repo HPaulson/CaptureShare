99122ead46d63fe90019cf307ab65f06649f98f0
Hunter Pauslon <hpaulson@smc.wtf>
2020-06-08 18:19:51 -0400

    Removed Packages
    - Removed acidently added node_modules, as well as built files in ./app

--- app/database/main.schema.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    Uploads: require('mongoose').model("uploads", require('mongoose').Schema({
-        _id: String,
-        uploadedAt: Date,
-        user: String,
-        fileType: String,
-        fileSize: Number
-    }))
-};
