d5a338c6fbbb1d27451ccd04cad40f4bd94a3c0e
Clayton Jensen <clayton.jensen2002@gmail.com>
2020-06-08 22:49:30 -0500

    Clean main.schema.ts

--- server/src/database/main.schema.ts
+++ server/src/database/main.schema.ts
@@ -1,11 +1,12 @@
 module.exports = {
-
-    Uploads: require('mongoose').model("uploads", require('mongoose').Schema({
-
-        _id: String,
-        uploadedAt: Date,
-        user: String,
-        fileType: String,
-        fileSize: Number
-    }))
-}
+    Uploads: 
+        require('mongoose').model('uploads',
+		require('mongoose').Schema({
+			_id: String,
+			uploadedAt: Date,
+			user: String,
+			fileType: String,
+			fileSize: Number
+		})
+	)
+};
